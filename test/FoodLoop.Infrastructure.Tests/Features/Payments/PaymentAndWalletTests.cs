8408bd50c569af1ad930f36e3ae978930ca260c6
Youssef Wagih <youssefwagih212@gmail.com>
2026-08-19 19:30:04 +0300

    test(payments): add missing using directive in PaymentAndWalletTests

--- test/FoodLoop.Infrastructure.Tests/Features/Payments/PaymentAndWalletTests.cs
+++ test/FoodLoop.Infrastructure.Tests/Features/Payments/PaymentAndWalletTests.cs
@@ -14,6 +14,7 @@ using FoodLoop.Infrastructure.Persistence;
 using FoodLoop.Infrastructure.Tests.TestSupport;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
