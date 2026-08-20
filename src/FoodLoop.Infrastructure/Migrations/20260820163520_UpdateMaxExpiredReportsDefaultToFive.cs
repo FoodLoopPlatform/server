11bcb1dd8d21411d297906b8ebc58fbdd8e6c50f
Youssef Wagih <youssefwagih212@gmail.com>
2026-08-20 19:36:20 +0300

    fix(db): add UpdateMaxExpiredReportsDefaultToFive migration to resolve PendingModelChangesWarning

---
+++ src/FoodLoop.Infrastructure/Migrations/20260820163520_UpdateMaxExpiredReportsDefaultToFive.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodLoop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMaxExpiredReportsDefaultToFive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxExpiredReportsBeforeDeactivation",
+                table: "SystemSettings",
+                type: "int",
+                nullable: false,
+                defaultValue: 5,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "SystemSettings",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "MaxExpiredReportsBeforeDeactivation",
+                value: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxExpiredReportsBeforeDeactivation",
+                table: "SystemSettings",
+                type: "int",
+                nullable: false,
+                defaultValue: 3,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 5);
+
+            migrationBuilder.UpdateData(
+                table: "SystemSettings",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "MaxExpiredReportsBeforeDeactivation",
+                value: 3);
+        }
+    }
+}
